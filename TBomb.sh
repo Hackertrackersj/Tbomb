963be29dd5825ca8fc5b9e89baf5f6b2d56c07d0
Sobhit jadoun <65443674+Hackertrackersj@users.noreply.github.com>
2020-12-11 17:21:33 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -97,18 +97,18 @@ do
     read ch
     clear
     if [ $ch -eq 1 ];then
-        echo -e "\e[1;34m wait please"
-        echo -e "\e[1;32m Subscribe Nitro Hacker Youtub Channel"
-        sleep 4
+        echo -e "\e[1;34m        wait please"
+        echo -e "\e[1;32m   Subscribe Nitro Hacker Youtube Channel"
+        sleep 5
         cd $HOME
         cd Tbomb
         python3 bomber.py
        
         exit
     elif [ $ch -eq 2 ];then
-        echo -e "\e[1;34m wait please"
-        echo -e "\e[1;32m Subscribe Nitro Hacker Youtub Channel"
-        sleep 4
+        echo -e "\e[1;34m          wait please"
+        echo -e "\e[1;32m    Subscribe Nitro Hacker Youtube Channel"
+        sleep 5
         cd $HOME
         cd Tbomb
         python3 bomber.py
@@ -116,9 +116,9 @@ do
         exit
         exit
     elif [ $ch -eq 3 ];then
-         echo -e "\e[1;34m wait please"
-        echo -e "\e[1;32m Subscribe Nitro Hacker Youtub Channel"
-         sleep 4
+         echo -e "\e[1;34m        wait please"
+        echo -e "\e[1;32m   Subscribe Nitro Hacker Youtube Channel"
+         sleep 5
          cd $HOME
         git clone https://github.com/ha-mrx/Emailbomb
          cd Emailbomb
