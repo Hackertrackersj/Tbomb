6882748f1045f0d3fed41eab2db0eaba57b0b722
Sobhit jadoun <65443674+Hackertrackersj@users.noreply.github.com>
2020-12-21 14:44:00 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -278,12 +278,12 @@ def selectnode(mode="sms"):
         check_for_updates()
         notifyen()
 
-        max_limit = {"sms": 5000, "call": 1000, "mail": 2000}
+        max_limit = {"sms": 500, "call": 15, "mail": 200}
         cc, target = "", ""
         if mode in ["sms", "call"]:
             cc, target = get_phone_info()
             if cc != "91":
-                max_limit.update({"sms": 5000})
+                max_limit.update({"sms": 100})
         elif mode == "mail":
             target = get_mail_info()
         else:
